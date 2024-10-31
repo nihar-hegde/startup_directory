e71885d0f87d0570d872fdb6f9d4bb6fc6f0559a
nihar_hegde <niharhegde163@gmail.com>
2024-10-31 15:59:19 +0530

    added navbar and auth using nextauth

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar";
+import { ReactNode } from "react";
+
+export default function layout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
+      {children}
+    </main>
+  );
+}
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-2xl">HOME</h1>{" "}
+    </>
+  );
+};
+
+export default Home;
